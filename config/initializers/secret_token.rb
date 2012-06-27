0c4a268a8615d4e3c2cbdb65a7c16706df05866f
Strand McCutchen <strand@bettermistak.es>
2012-06-27 15:33:53 -0700

    App creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Freely::Application.config.secret_token = '90eedf25a5d8488e3f2343396dc101077d6c001f152c3885d38c888fa6903efd0b7d21ab17a8029c57956c3b4e68f5bb7b238244cdc4bda8b013f969d4fa59e4'
